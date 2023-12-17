77106f81c43b8d17a6fcb2f6339ac14b6d200d10
Bjarne Braeuer <bjarne.braeuer@protonmail.com>
2023-12-17 22:20:04 +0100

    changed list of leagues

--- main.rb
+++ main.rb
@@ -3,57 +3,63 @@ require 'open-uri'
 require_relative 'match'
 
 def extract_data_from_league(url)
-  html = URI.open(url)
-  doc = Nokogiri::HTML(html, nil, 'UTF-8')
+  begin
+    html = URI.open(url)
+    doc = Nokogiri::HTML(html, nil, 'UTF-8')
 
-  match_counter = 0
-  matches = []
-  teams = []
-  results = []
+    match_counter = 0
+    matches = []
+    teams = []
+    results = []
 
-  doc.css('div').each do |con|
-    if con['class'] == 'kick__v100-gameCell__team__name'
-      teams << con.text
-    end
-  end
-
-  doc.css('div').each do |con|
-    if con['class'] == 'kick__v100-gameList__gameRow'
-      match_counter += 1
-    end
-  end
-
-  doc.css('div').each do |con|
-    if con['class'] == 'kick__v100-scoreBoard__scoreHolder '
-      con.css('div.kick__v100-scoreBoard__scoreHolder__score').each do |temp|
-        results << temp.text.strip
+    doc.css('div').each do |con|
+      if con['class'] == 'kick__v100-gameCell__team__name'
+        teams << con.text
       end
     end
-  end
 
-  match_counter.times do |i|
-    home = teams[i * 2]
-    away = teams[i * 2 + 1]
-    matches << Match.new(home, away)
-  end
-
-  # transform results into match logic
-  formatted_results = []
-  if results.length == match_counter * 2
-    results.each_slice(2) do |home_score, away_score |
-      formatted_results << "#{home_score}:#{away_score}"
+    doc.css('div').each do |con|
+      if con['class'] == 'kick__v100-gameList__gameRow'
+        match_counter += 1
+      end
     end
-  end
+
+    doc.css('div').each do |con|
+      if con['class'] == 'kick__v100-scoreBoard__scoreHolder '
+        con.css('div.kick__v100-scoreBoard__scoreHolder__score').each do |temp|
+          results << temp.text.strip
+        end
+      end
+    end
+
+    match_counter.times do |i|
+      home = teams[i * 2]
+      away = teams[i * 2 + 1]
+      matches << Match.new(home, away)
+    end
+
+    # transform results into match logic
+    formatted_results = []
+    if results.length == match_counter * 2
+      results.each_slice(2) do |home_score, away_score |
+        formatted_results << "#{home_score}:#{away_score}"
+      end
+    end
 
 
-  matches.each do |match|
-    # here we can identify the result and set it
-    match.set_result(formatted_results[matches.index(match)])
+    matches.each do |match|
+      # here we can identify the result and set it
+      match.set_result(formatted_results[matches.index(match)])
 
-    # print results
-    puts "#{match.home_team} - #{match.away_team} #{match.result}"
+      # print results
+      puts "#{match.home_team} - #{match.away_team} #{match.result}"
+    end
+  rescue => exception
+    puts "Error: #{exception}"
   end
 end
 
-league = "https://www.kicker.de/2-bundesliga/startseite"
-extract_data_from_league(league)
+urls = %w[https://www.kicker.de/bundesliga/startseite https://www.kicker.de/2-bundesliga/startseite https://www.kicker.de/3-liga/startseite https://www.kicker.de/regionalliga/startseite]
+urls.each do |url|
+  extract_data_from_league(url)
+end
